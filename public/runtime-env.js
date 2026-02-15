86d26eb84ab129d42a454e1422f7622ad17d8b45
djeon <daewoong.jeon@kt.com>
2026-02-15 10:21:03 +0900

    fix: fix cors error

--- public/runtime-env.js
+++ public/runtime-env.js
@@ -1,7 +1,7 @@
 // 런타임 환경 설정 - 배포 시 환경별로 변경 가능
 window.__runtime_config__ = {
   API_GROUP: "/api/v1",
-  USER_HOST: "http://localhost:8081",
+  USER_HOST: "http://:8081",
   ASSET_HOST: "http://localhost:8082",
   HOUSING_HOST: "http://localhost:8084",
   LOAN_HOST: "http://localhost:8083",
